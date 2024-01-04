fd9e83211c409e80669e30dd6ea029f40cd49dcd
Shubham Dnyaneshwar Kale <74854692+HeyItsShubhamKale@users.noreply.github.com>
2024-01-04 04:28:43 +0000

    created registered route

--- Backend/index.js
+++ Backend/index.js
@@ -14,6 +14,14 @@
     res.json("hello this is backend")
  });
 
+ app.get("/register", (req,res) =>{
+   const q =" SELECT * FROM test.user_master";
+   db.query (q,(err,data) =>{
+      if (err) return res.json(err)
+      else return res.json(data)
+   })
+ })
+
  app.listen(5000, ()=>{
 
     console.log("connected to backend")
